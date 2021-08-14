b32fe25ed5e9ef509034f96f4cc580c1eaa0233e
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2021-08-14 21:07:01 +0600

    domCSS.js

---
+++ domCSS.js
@@ -0,0 +1,16 @@
+//DOM CSS 
+
+//Styling CSS 
+
+//Initialize Variable 
+var paraTxt = document.querySelector('#paragraph');
+
+//function for add style 
+function add() {
+    paraTxt.classList.add("para-style");
+}
+
+//function for remove style 
+function remove() {
+    paraTxt.classList.remove('para-style');
+}
