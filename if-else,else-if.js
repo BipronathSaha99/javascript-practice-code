75396b22e9777b81236173298c04d7c537178edd
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2021-08-14 21:21:46 +0600

    nesteted-condition

---
+++ if-else,else-if.js
@@ -0,0 +1,62 @@
+//JS conditional 
+//if-else statement 
+// let numOne = prompt("Enter number one:");
+
+// let numTwo = prompt("Enter number two:");
+let numOne,numTwo;
+numOne =4;
+numTwo=6;
+
+
+/*
+let r =  confirm("Are you sure?");
+
+let txt; 
+
+if(r==true){
+  txt = numOne; 
+  console.log(txt);
+ 
+}
+else{
+  txt = numTwo;
+  console.log(txt);
+}
+*/
+
+/*
+if (numOne > numTwo){
+  console.log(numOne, "is greater");
+}
+else{
+console.log(numTwo, "is greater");
+}
+
+*/
+
+
+//else-if statement 
+/*
+if (numOne==numTwo){
+  console.log("Both numOne and numTwo are equal...");
+}
+else if(numOne>numTwo)
+{
+    console.log(numOne,"is the greatest number.");
+}
+else 
+{
+  console.log(numTwo,"is the greatest number. ");
+}
+*/
+
+// if if statement 
+
+if(numOne>numTwo){
+console.log(numOne,"is the greatest number.");
+}
+if (numOne<numTwo){
+console.log(numTwo,"is the greatest number.");
+} 
+
+
