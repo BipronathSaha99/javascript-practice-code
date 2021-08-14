445c245153fde0f60baddab2f44d366cdef71f6c
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2021-08-14 21:36:29 +0600

    type_converstion.js

---
+++ type_converstion.js
@@ -0,0 +1,20 @@
+/*
+You have two variable one of them is string type which value is 5628, and another one is integer which
+is 255, make add operation between them, and keep the answer in integer type variable, and finally
+print the result. 
+*/
+// solution is given bellow:
+
+
+let str = "5628", num = 255;
+let sum = Number(str) + num;
+console.log(`The result is = ${sum}`);
+
+//by taking user defined variable
+/*
+let stringValue = prompt("Enter the string value: ");
+let num = parseInt(prompt("Enter the integer value: "));
+let convstringValue= Number(stringValue);
+let add = convstringValue+num;
+console.log(`The result is =${add}`);
+*/
