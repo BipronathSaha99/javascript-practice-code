c1853e8e0a2b7194f793da0e9eb28f70363d6c21
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2021-08-14 21:00:56 +0600

    area_shape.js

---
+++ area_shapes.js
@@ -0,0 +1,42 @@
+// Area of Shapes 
+
+// Area of a Triangle
+/*
+//input
+let base, height, area;
+base = parseFloat(prompt("Enter the Base:"));
+height =parseFloat(prompt("Enter the Height: "));
+
+//calculation
+area = (base*height)/2;
+
+//output
+console.log(`Area of a Triangle is = ${area} sq.unit`);
+*/
+// Area of Parallelogram
+/*
+//input
+let base, height, area;
+base = parseFloat(prompt("Enter the Base:"));
+height =parseFloat(prompt("Enter the Height: "));
+
+//calculation
+area = (base*height);
+
+//output
+console.log(`Area of a Parallelogram is = ${area} sq.unit`);
+*/
+
+// Area of Circle
+
+//input
+let radius, area;
+const pi = 3.14159;
+
+radius = parseFloat(prompt("Enter the radius: "));
+
+//calculation
+area = pi*(radius**2);
+
+//output
+console.log(`Area of a Circle is = ${area} sq.unit`);
