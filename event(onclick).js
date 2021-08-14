f3e591f641763c826c91504e7582da1c65b48808
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2021-08-14 21:15:17 +0600

    event(onclick).js

---
+++ event(onclick).js
@@ -0,0 +1,13 @@
+//Event Handeller 
+
+function btnOneTxt() {
+    alert('Hello Bipronath');
+}
+
+function btnTwoTxt() {
+    document.querySelector(".btnTwo"). innerHTML = 'Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. '
+}
+
+function myPic() {
+    document.querySelector("#pic").src = '/assets/mountains.jpg';
+}
