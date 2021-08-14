6184069e7644634bec74145a6e51ce6fe8a5bd80
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2021-08-14 21:35:04 +0600

    ternary_operator.js

---
+++ ternary_operator.js
@@ -0,0 +1,9 @@
+// let numOne = 4, numTwo=6;
+
+// let result = (numOne>numTwo)? "numOne is the greatest number":"numTwo is the greatest number"
+// console.log(result);
+
+let num = 5;
+let result = (num%2==0)? "even number":"odd number";
+console.log(result);
+
