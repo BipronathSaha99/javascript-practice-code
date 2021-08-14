261369d092d5158cbe2f0ba4cb2a558f27985543
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2021-08-14 21:37:29 +0600

    variable.js

---
+++ variable.js
@@ -0,0 +1,66 @@
+//variable in js
+
+//variable with var keyword
+/*
+var numberOne=20,numberTwo=30,sumOfNumbers;
+sumOfNumbers=numberOne+numberTwo;
+console.log(sumOfNumbers);
+*/
+//var : it can be declared the values and can be redeclared the vaues multiple times within its scope.
+
+/*
+var userText="Hello, This is Bipronath"
+
+//logic
+if (true){
+   var  userText="Hey, Are you gonna market?"
+    console.log(userText)
+}
+*/
+/*
+var userName="Bipronath"
+
+function sayHi(){
+    var userName="Saha"
+    if (true){
+        console.log(userName);
+    }
+}
+sayHi();
+console.log("Outside the function")
+console.log(userName);
+
+//var declaration is global scoped and functional scope/local scope
+
+
+*/
+
+//let declaration
+// let userName='Bipronath';
+
+// if(true){
+//     let userName="Saha";
+//     console.log(userName);
+// }
+// console.log(userName);
+
+// function sayHi(){
+//     let userName="Saha"
+//     if (true){
+//         console.log(userName);
+//     }
+// }
+// sayHi();
+// console.log("Outside the function")
+// console.log(userName);
+
+//let variables can be updated but not re-declared;
+
+const userName="Pratap";
+
+if(true){
+   
+    console.log(userName);
+}
+
+//const variables can neither be updated nor re-declared. 
