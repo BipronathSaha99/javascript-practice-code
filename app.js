5c43f7a51adc3f8142b8aa175a6b9ac1745320ac
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2021-08-14 20:59:56 +0600

    app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+// // let msg="Hello!";
+
+// // //The code for function
+// // /* multiline comments*/
+// // [1,2,4,5].forEach(
+// //     value=>{
+// //         console.log(msg)
+// //     }
+// // );
+
+// /*code block: Those parts are called code 
+// blocks which are started in between the carli brackets*/
+// let ageDB=10;
+// let userAge=19;
+
+// if(userAge>ageDB){
+//     console.log("You can registerd.");
+// }
+// else{
+//     console.log("You can't registerd.");
+
+// }
+
+//variable
+//var can reassign the values and declares multiple times.
+// var name="Bipronath"
+/*if(true){
+    // let name="saha"
+    console.log(name);
+}*/
+
+// let x=10;
+// if(true){
+//     x=20;
+//     console.log(x);
+// }
+
+/*
+var declarations are globally scoped or function scoped 
+while let and const are block scoped. var variables can be updated and re-declared 
+within its scope; let variables can be updated but not re-declared; 
+const variables can neither be updated nor re-declared. 
+They are all hoisted to the top of their scope.
+
+
+*/
