621a797288cd321640ba0d61a2da1e7534f179cb
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2021-08-14 21:28:09 +0600

    mathObj.js

---
+++ mathObj.js
@@ -0,0 +1,22 @@
+// Math Object
+let num = 12.23;
+
+console.log(Math.round(num)); // round() method returns the nearest integer value. 
+console.log(Math.ceil(num)); // ceil() method returns up to its nearest integer value
+console.log(Math.floor(num));// floor() returns down to its nearest integer value
+console.log(Math.trunc(num)); // trnc() method returns the nearest integer value 
+
+
+num = 4 ;
+
+console.log(Math.pow(num,4));
+console.log(Math.log(num));
+console.log(Math.log10(num));
+console.log(Math.LN10)
+
+
+console.log(Math.max(2,4));
+console.log(Math.min(4,1))
+console.log(Math.sin(30*Math.PI/180));
+console.log(Math.cos(30*Math.PI/180));
+console.log(Math.asin(1));
